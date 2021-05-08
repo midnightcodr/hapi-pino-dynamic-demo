435277ed7e0c881c037401f28087f799782957c0
Rico Chen <ricochen27@gmail.com>
2021-05-08 15:24:30 -0400

    Update routes.js
    updated comment in routes.js

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports = [
     handler (request) {
       // this won't show when inital default level is set to 'info'
       // to change log level on the fly
-      // echo -n debug > /tmp/PID.loglevel
+      // echo -n debug > /tmp/loglevel.PID
       // kill -SIGUSR1 PID
       // where PID is the process id of the main process
       request.server.logger.debug('some debug info here')
